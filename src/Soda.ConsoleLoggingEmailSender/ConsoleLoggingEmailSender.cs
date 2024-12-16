2a4d47a4c474e51de8dddfb702eff42ede13dc47
Chad Tolkien <chad@sodadigital.com.au>
2024-12-16 12:06:51 +1000

    init

---
+++ src/Soda.ConsoleLoggingEmailSender/ConsoleLoggingEmailSender.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Logging;
+
+namespace Soda.DummyEmailSender;
+
+public class ConsoleLoggingEmailSender : IEmailSender
+{
+    private readonly ILogger<ConsoleLoggingEmailSender> _logger;
+
+    protected ConsoleLoggingEmailSender(ILogger<ConsoleLoggingEmailSender> logger)
+    {
+        _logger = logger;
+    }
+
+    public virtual Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        _logger.LogInformation("Sending email to {email} with subject {subject}", email, subject);
+        _logger.LogInformation(htmlMessage);
+
+        return Task.CompletedTask;
+    }
+}
